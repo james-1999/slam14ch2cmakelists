b314f48acf8c9f261b5b10a0c3867d0d5553138f
james-1999 <59297917+james-1999@users.noreply.github.com>
2022-09-21 16:09:26 +0800

    测试每个层级的Makefile对应于每个层级的CMakelists

---
+++ 非库/build/CMakeFiles/sayhellotop.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sayhellotop.dir/src/hello.cpp.o"
+  "CMakeFiles/sayhellotop.dir/src/useHello.cpp.o"
+  "sayhellotop"
+  "sayhellotop.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sayhellotop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 非库/build/out/CMakeFiles/sayhello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sayhello.dir/hello.cpp.o"
+  "CMakeFiles/sayhello.dir/useHello.cpp.o"
+  "sayhello"
+  "sayhello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sayhello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
